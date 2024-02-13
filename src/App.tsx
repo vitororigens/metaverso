99a1a86c4428d063d63b021bb6dc2ae69732329b
vitororigens <vitorabreufjb@hotmail.com>
2024-02-13 13:25:16 -0300

    alteração no app

--- src/App.tsx
+++ src/App.tsx
@@ -69,13 +69,6 @@ function App() {
     <div className="App">
       <style>
         {`
-        .App {
-          display: flex;
-          text-align: center;
-          justify-content: center;
-          align-items: center;
-          width: 100vw;
-        }
         .Container {
           position: relative; 
           display: flex;
